60dfad2553b639af48626363f3fc9ded87337f12
Matt Conway <mattconway55@gmail.com>
2019-12-08 23:59:48 -0500

    setup summoner search route for proxy

---
+++ README.md
@@ -0,0 +1,3 @@
+## LoL Tracker Proxy
+
+Used to proxy Riot API requests from the front-end to the API servers in a way that keeps our API key private
---
+++ proxy.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+// Summoner represents a response from the Riot API Summoner endpoint
+type Summoner struct {
+	ProfileIconID int
+	Name          string
+	Puuid         string
+	SummonerLevel int
+	AccountID     string
+	ID            string
+	RevisionDate  uint64
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	riotAPIKey := os.Getenv("RIOT_API_KEY")
+
+	router := gin.Default()
+
+	router.Use(cors.Default())
+
+	router.GET("/lol/summoner/v4/summoners/by-name/:name", func(c *gin.Context) {
+		summonerName := c.Param("name") // TODO: should see if this is safe to just make requests with. i.e : can users inject something here?
+		client := &http.Client{}
+		req, err := http.NewRequest("GET", fmt.Sprintf("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/%s", summonerName), nil)
+		if err != nil {
+			fmt.Printf("error creating request object: %v", err)
+			c.JSON(500, gin.H{
+				"error": err,
+			})
+		}
+
+		req.Header.Add("X-Riot-Token", riotAPIKey)
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Printf("error making request: %v", err)
+			c.JSON(500, gin.H{
+				"error": err,
+			})
+		}
+
+		defer resp.Body.Close()
+
+		summoner := Summoner{}
+		json.NewDecoder(resp.Body).Decode(&summoner)
+
+		c.JSON(200, summoner)
+	})
+
+	router.Run()
+}
